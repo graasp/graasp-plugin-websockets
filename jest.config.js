f95763b4f88626396a6c43634513fd0427b05bb1
Alexandre Chau <accounts@chau.moe>
2021-06-09 23:58:35 +0200

    test: add sketchy jest config (runtime TypeError)

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,15 @@ module.exports = {
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/", "/test/"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   verbose: true,
+  moduleNameMapper: {
+    "graasp": "<rootDir>/node_modules/@types/graasp/index.d.ts"
+  },
+  transformIgnorePatterns: [
+    "node_modules/@types/(?!graasp)"
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    }
+  }
 };
